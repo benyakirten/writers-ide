5cdf9797e37d74a7a90de20a7dfe02bc0a5fa501
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2025-02-13 22:01:47 -0700

    Finish basic tab state tests [ci skip]

--- src/lib/editor/state/tab-state.svelte.test.ts
+++ src/lib/editor/state/tab-state.svelte.test.ts
@@ -9,49 +9,82 @@ describe('TabState', () => {
 		tabState = new TabState();
 	});
 
-	it('should create a new tab', () => {
-		const id = tabState.createTab();
-		expect(tabState.windows).toHaveLength(1);
-		expect(tabState.windows[0].id).toBe(id);
+	describe('createTab', () => {
+		it('should create a new tab with no view and return the id of the window', () => {
+			const id = tabState.createTab();
+			expect(tabState.windows).toHaveLength(1);
+			expect(tabState.windows[0].id).toBe(id);
+			expect(tabState.windows[0].view).toBeUndefined();
+		});
 	});
 
-	it('should register an editor', () => {
-		const id = tabState.createTab();
-		const unregister = tabState.registerEditor(id, null);
-		expect(tabState.windows[0].view).toBeNull();
-		unregister();
-		expect(tabState.windows).toHaveLength(0);
+	describe('registerEditor', () => {
+		it('should register a tab, assign the data to the view and return a function that removes the editor', () => {
+			const id = tabState.createTab();
+			const symbol = Symbol('view');
+			const unregister = tabState.registerEditor(id, symbol as unknown as null);
+
+			expect(tabState.windows[0].view).toBe(symbol);
+
+			const id2 = tabState.createTab();
+			const id3 = tabState.createTab();
+
+			unregister();
+			expect(tabState.windows).toHaveLength(2);
+			expect(tabState.windows[0].id).toBe(id2);
+			expect(tabState.windows[1].id).toBe(id3);
+		});
 	});
 
-	it('should remove a tab', () => {
-		const id = tabState.createTab();
-		tabState.remove(id);
-		expect(tabState.windows).toHaveLength(0);
+	describe('remove', () => {
+		it('should remove a tab', () => {
+			const id = tabState.createTab();
+			const id2 = tabState.createTab();
+
+			tabState.remove(id);
+
+			expect(tabState.windows).toHaveLength(1);
+			expect(tabState.windows[0].id).toBe(id2);
+		});
 	});
 
-	it('should swap two tabs by id', () => {
-		const id1 = tabState.createTab();
-		const id2 = tabState.createTab();
-		const result = tabState.swap(id1, id2);
-		expect(result).toBe(true);
-		expect(tabState.windows[0].id).toBe(id2);
-		expect(tabState.windows[1].id).toBe(id1);
+	describe('swap', () => {
+		it('should swap two tabs by id', () => {
+			const id1 = tabState.createTab();
+			const id2 = tabState.createTab();
+			const result = tabState.swap(id1, id2);
+			expect(result).toBe(true);
+			expect(tabState.windows[0].id).toBe(id2);
+			expect(tabState.windows[1].id).toBe(id1);
+		});
 	});
 
-	it('should swap two tabs by index', () => {
-		tabState.createTab();
-		tabState.createTab();
-		const result = tabState.swapByIndex(0, 1);
-		expect(result).toBe(true);
-		expect(tabState.windows[0].id).toBe(tabState.windows[1].id);
+	describe('swapByIndex', () => {
+		it('should swap two tabs by index', () => {
+			const id1 = tabState.createTab();
+			const id2 = tabState.createTab();
+
+			expect(tabState.windows).toHaveLength(2);
+			expect(tabState.windows[0].id).toBe(id1);
+			expect(tabState.windows[1].id).toBe(id2);
+
+			const result = tabState.swapByIndex(0, 1);
+			expect(result).toBe(true);
+			expect(tabState.windows[0].id).toEqual(id2);
+			expect(tabState.windows[1].id).toEqual(id1);
+		});
 	});
 
-	it('should move a tab to a new position', () => {
-		const id1 = tabState.createTab();
-		const id2 = tabState.createTab();
-		const result = tabState.moveToPosition(id1, 1);
-		expect(result).toBe(true);
-		expect(tabState.windows[0].id).toBe(id2);
-		expect(tabState.windows[1].id).toBe(id1);
+	describe('moveToPosition', () => {
+		it('should move a tab to a new position', () => {
+			const id1 = tabState.createTab();
+			const id2 = tabState.createTab();
+			const id3 = tabState.createTab();
+			const result = tabState.moveToPosition(id1, 1);
+			expect(result).toBe(true);
+			expect(tabState.windows[0].id).toBe(id2);
+			expect(tabState.windows[1].id).toBe(id1);
+			expect(tabState.windows[2].id).toBe(id3);
+		});
 	});
 });
