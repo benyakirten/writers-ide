481ba829415b66346aa5c176c397df5cd8041ea9
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2025-03-09 23:30:44 -0600

    Add draggable component [ci skip]

---
+++ src/lib/dnd/Draggable.svelte
@@ -0,0 +1,14 @@
+<script lang="ts" generics="T extends Record<string, unknown>">
+	import { useDraggable } from '@dnd-kit-svelte/core';
+	import { CSS } from '@dnd-kit-svelte/utilities';
+
+	let { id, data }: { id: string; data: T } = $props();
+
+	const { transform, listeners, attributes, node } = useDraggable({ id, data });
+
+	const style = $derived(
+		transform.current ? `transform: ${CSS.Translate.toString(transform.current)};` : ''
+	);
+</script>
+
+<div {style} bind:this={node.current} {...listeners.current} {...attributes.current}>Drag me</div>
