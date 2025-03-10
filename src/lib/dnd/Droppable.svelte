6aa97722eaf4d1e367f816945fb71724bfa4d797
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2025-03-09 23:25:06 -0600

    Create the droppable component [ci skip]

---
+++ src/lib/dnd/Droppable.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { useDroppable, type UseDroppableArguments } from '@dnd-kit-svelte/core';
+	import type { Snippet } from 'svelte';
+
+	interface DroppableProps extends UseDroppableArguments {
+		children: Snippet<[ReturnType<typeof useDroppable>]>;
+	}
+
+	let { children, ...rest }: DroppableProps = $props();
+
+	const droppable = useDroppable(rest);
+</script>
+
+<div bind:this={droppable.node.current}>
+	{@render children(droppable)}
+</div>
--- src/lib/editor/Editor.svelte
+++ src/lib/editor/Editor.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-	import MainView from './MainView.svelte';
-	import HorizontalBarState from './state/horizontal-bar-state.svelte.js';
+	import {
+		DndContext,
+		KeyboardSensor,
+		MouseSensor,
+		TouchSensor,
+		useSensor,
+		useSensors,
+		type DragEndEvent
+	} from '@dnd-kit-svelte/core';
+
 	import VerticalBarState, { VerticalBarPosition } from './state/vertical-bar-state.svelte.js';
+	import HorizontalBarState from './state/horizontal-bar-state.svelte.js';
+	import { HorizontalBarPosition } from './state/horizontal-bar-state.svelte.js';
+	import FloaterBarState from './state/floater-state.svelte.js';
+
+	import MainView from './MainView.svelte';
 	import VerticalSlice from './bars/VerticalSlice.svelte';
 	import HorizontalSlice from './bars/HorizontalSlice.svelte';
-	import { HorizontalBarPosition } from './state/horizontal-bar-state.svelte.js';
 	import FloaterBar from './bars/FloaterBar.svelte';
 	import VerticalBaseBar from './bars/VerticalBaseBar.svelte';
-	import FloaterBarState from './state/floater-state.svelte.js';
 	import HorizontalBaseBar from './bars/HorizontalBaseBar.svelte';
 
 	function resize(e: MouseEvent) {
@@ -20,73 +31,85 @@
 		HorizontalBarState.endResize();
 		FloaterBarState.stopDragging();
 	}
+
+	const sensors = useSensors(
+		useSensor(TouchSensor),
+		useSensor(KeyboardSensor),
+		useSensor(MouseSensor)
+	);
+
+	function handleDragEnd(e: DragEndEvent) {
+		console.log(e);
+	}
 </script>
 
-<div
-	bind:this={FloaterBarState.root}
-	class="overlay"
-	onmouseupcapture={() => endResize()}
-	onmousemovecapture={(event) => resize(event)}
->
-	<HorizontalBaseBar />
-	{#each FloaterBarState.visibleBars as bar, index (bar.id)}
-		<FloaterBar {bar} {index} items={bar.data.items} />
-	{/each}
-	{#each HorizontalBarState.windowBlockStart as bar, index (bar.id)}
-		<HorizontalSlice
-			{bar}
-			position={HorizontalBarPosition.WindowBlockStart}
-			{index}
-			items={bar.data.items}
-		/>
-	{/each}
-	<div class="main-container">
-		<VerticalBaseBar />
-		{#each VerticalBarState.inlineStart as bar, index (bar.id)}
-			<VerticalSlice
+<DndContext {sensors} onDragEnd={handleDragEnd}>
+	<div
+		bind:this={FloaterBarState.root}
+		class="overlay"
+		onmouseupcapture={() => endResize()}
+		onmousemovecapture={(event) => resize(event)}
+	>
+		<HorizontalBaseBar />
+		{#each FloaterBarState.visibleBars as bar, index (bar.id)}
+			<FloaterBar {bar} {index} items={bar.data.items} />
+		{/each}
+		{#each HorizontalBarState.windowBlockStart as bar, index (bar.id)}
+			<HorizontalSlice
 				{bar}
-				position={VerticalBarPosition.InlineStart}
+				position={HorizontalBarPosition.WindowBlockStart}
 				{index}
 				items={bar.data.items}
 			/>
 		{/each}
-		<main class="main">
-			{#each HorizontalBarState.editorBlockStart as bar, index (bar.id)}
-				<HorizontalSlice
+		<div class="main-container">
+			<VerticalBaseBar />
+			{#each VerticalBarState.inlineStart as bar, index (bar.id)}
+				<VerticalSlice
 					{bar}
-					position={HorizontalBarPosition.EditorBlockStart}
+					position={VerticalBarPosition.InlineStart}
 					{index}
 					items={bar.data.items}
 				/>
 			{/each}
-			<MainView />
-			{#each HorizontalBarState.editorBlockEnd as bar, index (bar.id)}
-				<HorizontalSlice
+			<main class="main">
+				{#each HorizontalBarState.editorBlockStart as bar, index (bar.id)}
+					<HorizontalSlice
+						{bar}
+						position={HorizontalBarPosition.EditorBlockStart}
+						{index}
+						items={bar.data.items}
+					/>
+				{/each}
+				<MainView />
+				{#each HorizontalBarState.editorBlockEnd as bar, index (bar.id)}
+					<HorizontalSlice
+						{bar}
+						position={HorizontalBarPosition.EditorBlockEnd}
+						{index}
+						items={bar.data.items}
+					/>
+				{/each}
+			</main>
+			{#each VerticalBarState.inlineEnd as bar, index (bar.id)}
+				<VerticalSlice
 					{bar}
-					position={HorizontalBarPosition.EditorBlockEnd}
+					position={VerticalBarPosition.InlineEnd}
 					{index}
 					items={bar.data.items}
 				/>
 			{/each}
-		</main>
-		{#each VerticalBarState.inlineEnd as bar, index (bar.id)}
-			<VerticalSlice
+		</div>
+		{#each HorizontalBarState.windowBlockEnd as bar, index (bar.id)}
+			<HorizontalSlice
 				{bar}
-				position={VerticalBarPosition.InlineEnd}
+				position={HorizontalBarPosition.WindowBlockEnd}
 				{index}
 				items={bar.data.items}
 			/>
 		{/each}
 	</div>
-	{#each HorizontalBarState.windowBlockEnd as bar, index (bar.id)}
-		<HorizontalSlice
-			{bar}
-			position={HorizontalBarPosition.WindowBlockEnd}
-			{index}
-			items={bar.data.items}
-		/>
-	{/each}
-</div>
+</DndContext>
 
 <style>
 	.overlay {
