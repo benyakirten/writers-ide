b713abd8029d7d85f403d75df737c7722342ed6d
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2025-10-15 21:27:52 -0600

    complete first draft of pagination queue [ci skip]

--- src/lib/components/pagination/queue.svelte.ts
+++ src/lib/components/pagination/queue.svelte.ts
@@ -1,7 +1,7 @@
 import { ArrayStream } from 'array-stream-plus';
 
 import PageLayout from '$lib/editor/state/page-layout.svelte';
-import type { ToPaginateRange } from '$lib/editor/state/page-observer.svelte';
+import { PageObserver, type ToPaginateRange } from '$lib/editor/state/page-observer.svelte';
 import Editors from '@/editor/prosemirror/prose-mirror-editor.svelte';
 
 type ToPaginate = {
@@ -11,46 +11,65 @@ type ToPaginate = {
 };
 
 type CurrentPagination = ToPaginate & {
+	time: number;
 	progress: number;
+	iter: Generator<number, void, unknown>;
 };
 
 export class PaginationQueue {
-	upcomingPagination: ToPaginate[] = $state([]);
-	currentPagination: CurrentPagination | null = $state(null);
-	constructor(paginationList: ToPaginateRange[]) {
-		$effect.root(() => {
-			if (paginationList.length === 0) {
-				this.upcomingPagination = [];
-				this.currentPagination = null;
-				return;
-			}
+	PAGINATION_ITERATION_TIMER_IN_MS = 50;
+	MAX_ITERS_BEFORE_SHOWING_MODAL = 2;
 
-			this.upcomingPagination = paginationList
-				.slice(0, -1)
-				.map(([id, from, to]) => ({ name: id, from, to }));
-			this.setCurrentPagination(paginationList[paginationList.length - 1]);
-		});
+	paginationQueue = $derived(
+		PageObserver.toPaginate.slice(1).map((item) => this.processToPaginateItem(item)),
+	);
+	currentPagination: CurrentPagination | null = $derived(
+		this.setCurrentPagination(PageObserver.toPaginate.at(0)),
+	);
+
+	constructor() {}
+
+	processToPaginateItem([id, from, to]: ToPaginateRange): ToPaginate {
+		const editor = Editors.get(id);
+		const name = editor ? editor.name : 'Unknown';
+		return { name, from, to };
 	}
 
-	setCurrentPagination(pageRange: ToPaginateRange) {
-		// TODO
+	setCurrentPagination(range?: ToPaginateRange): CurrentPagination | null {
+		if (!range) {
+			return null;
+		}
+
+		const [id, from, to] = range;
+		const editor = Editors.get(id);
+
+		const name = editor?.name ?? 'Unknown';
+		const iter = this.getPagePaginationIterator(id, from, to);
+
+		return {
+			name,
+			from,
+			to,
+			progress: 0,
+			time: 0,
+			iter,
+		};
 	}
 
 	*getPagePaginationIterator(id: string, from: number, to: number) {
 		const editor = Editors.get(id);
 		if (!editor) {
-			return null;
+			return;
 		}
 
 		const paginationIterator = PageLayout.paginateRange(id, editor.view, from, to);
-		const stream = new ArrayStream(paginationIterator).batch({ timeout: 50 });
+		const stream = new ArrayStream(paginationIterator).batch({
+			timeout: this.PAGINATION_ITERATION_TIMER_IN_MS,
+		});
 
 		for (const pageNums of stream.read()) {
 			const lastPage = pageNums[pageNums.length - 1];
-			const numerator = lastPage - from + 1;
-			const denominator = to - from;
-
-			yield numerator / denominator;
+			yield lastPage;
 		}
 	}
 }
